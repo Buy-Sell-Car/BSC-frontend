432c45f20f5529246b2491f9aff94e2677b1813d
edamiyan <ed.amiyan@ya.ru>
2021-04-25 23:17:15 +0300

    Add advert item component

---
+++ bsc/src/components/AdvertItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <li>
+        <div>{{ advert.title }}</div>
+        <div>Цена: {{ advert.price }} Рублей</div>
+        <hr>
+    </li>
+</template>
+
+<script>
+export default {
+    props: {
+        advert: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
