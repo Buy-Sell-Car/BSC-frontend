8a17b3b321a36a2b71d9b27f66f1dd6da1bbb5e2
edamiyan <ed.amiyan@ya.ru>
2021-04-30 01:27:35 +0300

    Add card with info

--- bsc/src/views/Detail.vue
+++ bsc/src/views/Detail.vue
@@ -5,21 +5,17 @@
     <!-- Карусель -->
       <div class="row">
         <div class="col-6">
-          <div>
             <b-carousel
               id="carousel-1"
               v-model="slide"
-              :interval="4000"
               controls
               indicators
               background="#ababab"
               img-width="1024"
               img-height="480"
-              style="text-shadow: 1px 1px 2px #333;"
               @sliding-start="onSlideStart"
               @sliding-end="onSlideEnd"
             >
-
               <b-carousel-slide v-for="img in advert.advertimage_set">
                 <template #img>
                   <img
@@ -31,23 +27,79 @@
                 </template>
               </b-carousel-slide>
             </b-carousel>
-          </div>
-
         </div>
-
-              <!-- <b-carousel-slide v-for="img in advert.advertimage_set">
-                <template #img>
-                <img
-                  width="720"
-                  height="480"
-                  :src=img.image
-                >
-                </template>
-              </b-carousel-slide>
-            </b-carousel> -->
+        <div class="col-6">
+          <ul class="СardInfo">
+              <li class="CardInfo_row">
+                <span class="CardRow_label">Год выпуска </span>
+                <span class="CardRow_param"> {{ advert.prod_year }}</span>
+              </li>
+              <li class="CardInfo_row">
+                <span class="CardRow_label">Пробег </span>
+                <span class="CardRow_param">{{ advert.mileage }}</span>
+              </li>
+              <li class="CardInfo_row">
+                <span class="CardRow_label">Кузов </span>
+                <span class="CardRow_param">{{ advert.carbody }}</span>
+              </li>
+              <li class="CardInfo_row">
+                <span class="CardRow_label">Цвет </span>
+                <span class="CardRow_param">{{ advert.color }}</span>
+              </li>
+              <li class="CardInfo_row">
+                <span class="CardRow_label">Двигатель </span>
+                <span class="CardRow_param">{{ advert.power }} л.с. / {{ advert.fuel }} </span>
+              </li>
+              <li class="CardInfo_row">
+                <span class="CardRow_label">Коробка </span>
+                <span class="CardRow_param">{{ advert.transmission }}</span>
+              </li>
+              <li class="CardInfo_row">
+                <span class="CardRow_label">Привод </span>
+                <span class="CardRow_param">{{ advert.drive }}</span>
+              </li>
+              <li class="CardInfo_row">
+                <span class="CardRow_label">Владельцы </span>
+                <span class="CardRow_param">{{ advert.owners }}</span>
+              </li>
+          </ul>
+        </div>
       </div>
   </div>
 </template>
+
+<style scoped>
+.СardInfo {
+  font-size: 15px;
+  flex-wrap: wrap;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+.CardInfo_row {
+  display: flex;
+}
+
+.CardRow_label {
+  flex-shrink: 0;
+  width: 140px;
+  line-height: 22px;
+  display: block;
+  padding: 3px 0;
+  text-overflow: ellipsis;
+  color: grey;
+}
+.CardRow_param {
+    flex-grow: 1;
+    padding-left: 8px;
+    color: #000;
+    line-height: 22px;
+    display: block;
+    padding: 3px 0;
+    text-overflow: ellipsis;
+}
+</style>
 
 <script>
 import axios from 'axios'
