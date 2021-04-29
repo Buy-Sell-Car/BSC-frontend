c6039b12ce9466693c96adfcb0fdbb7c02941507
Andrei Shestakov <75143257+shestakovar@users.noreply.github.com>
2021-04-30 02:48:58 +0300

    Connect login page to vuex token store

--- bsc/src/views/Login.vue
+++ bsc/src/views/Login.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="mt-2">
-    <b-form>
+    <b-alert v-model="showDismissibleAlert" variant="danger">Неверное имя пользователя или пароль</b-alert>
+    <b-form @submit.prevent="login">
       <b-form-group id="input-group-1" label="Имя пользователя:" label-for="input-1">
         <b-form-input
           id="input-1"
+          v-model="username"
           type="text"
           placeholder="Имя пользователя"
           required
@@ -13,6 +15,7 @@
       <b-form-group id="input-group-2" label="Пароль:" label-for="input-2">
         <b-form-input
           id="input-2"
+          v-model="password"
           type="password"
           placeholder="Пароль"
           required
@@ -25,3 +28,29 @@
     <b-button block to="/">Зарегистрироваться</b-button>
   </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        username: '',
+        password: '',
+        showDismissibleAlert: false
+      }
+    },
+    methods: {
+      login() {
+        this.$store.dispatch('userLogin', {
+          username: this.username,
+          password: this.password
+        })
+        .then(() => {
+            this.$router.push({name: 'Home'})
+        })
+        .catch(err => {
+            this.showDismissibleAlert = true
+        })
+      }
+    }
+  }
+</script>
