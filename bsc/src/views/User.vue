7cebfbff870900f55b365aaf25421adaa04c57c1
Andrei Shestakov <75143257+shestakovar@users.noreply.github.com>
2021-04-30 21:55:02 +0300

    Make user page human-readable

--- bsc/src/views/User.vue
+++ bsc/src/views/User.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>{{user}}</div>
+  <div v-if="user">
+      <div>Ник: {{ user.username }}</div>
+      <div>Имя: {{ user.first_name }}</div>
+      <div>Фамилия: {{ user.last_name }}</div>
+      <div>Электронная почта: {{ user.email }}</div>
+      <div>Город: {{ user.city }}</div>
+      <div>Телефон: {{ user.tel }}</div>
+  </div>
 </template>
 
 <script>
