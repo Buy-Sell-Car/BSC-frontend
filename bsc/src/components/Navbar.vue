f0d878d184e891bf31d5168c967ca9ab85dba328
Andrei Shestakov <75143257+shestakovar@users.noreply.github.com>
2021-04-30 04:32:01 +0300

    Tweak navbar

--- bsc/src/components/Navbar.vue
+++ bsc/src/components/Navbar.vue
@@ -4,6 +4,8 @@
         <b-navbar toggleable="lg" type="dark" variant="info">
           <b-navbar-brand href="#">BSC</b-navbar-brand>
 
+          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
           <b-collapse id="nav-collapse" is-nav>
             <b-navbar-nav>
               <b-nav-item to="/">Объявления</b-nav-item>
@@ -12,10 +14,16 @@
 
             <!-- Right aligned nav items -->
             <b-navbar-nav class="ml-auto">
-              <b-nav-item to="/">{{ $store.state.username }}</b-nav-item>
               <b-nav-item :to="{name: 'login'}" v-if="!$store.getters.loggedIn">Войти</b-nav-item>
-              <b-nav-item @click="logout" v-else>Выйти</b-nav-item>
-              <b-nav-item to="/">Зарегистрироваться</b-nav-item>
+              <b-nav-item-dropdown right v-else>
+                <!-- Using 'button-content' slot -->
+                <template #button-content>
+                    <em>{{ $store.state.username }}</em>
+                </template>
+                <b-dropdown-item href="#">Профиль</b-dropdown-item>
+                <b-dropdown-item @click="logout">Выйти</b-dropdown-item>
+                </b-nav-item-dropdown>
+            
             </b-navbar-nav>
           </b-collapse>
         </b-navbar>
