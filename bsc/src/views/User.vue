38094313a808cacce9f96b8ef454f02e6ce9cea0
Andrei Shestakov <75143257+shestakovar@users.noreply.github.com>
2021-05-09 23:43:14 +0300

    Connect edit userinfo to backend

--- bsc/src/views/User.vue
+++ bsc/src/views/User.vue
@@ -1,45 +1,38 @@
 <template>
 <div>
   <div v-if="user">
+    <b-alert v-model="showDismissibleAlert" variant="danger">{{this.err}}</b-alert>
+    <b-alert v-model="showSuccess" variant="success">Обновлено!</b-alert>
     <h3 class="mt-5">Редактирование профиля</h3>
-    <div class="userEdit">
-      <div class="row mt-4">
+    <div class="userEdit py-4">
+      <div class="row">
           <div class="text-right col-4 mt-2">Логин:</div>
-          <b-form-input class="col-3" v-model="username" readonly></b-form-input>
-          <button class="editButton col-1 ml-2"><b-icon-pencil></b-icon-pencil></button>
-      </div>
-      <div class="row mt-3">
-          <div class="text-right col-4 mt-2">Имя:</div>
-          <b-form-input class="col-3" v-model="username" readonly></b-form-input>
-          <button class="editButton col-1 ml-2"><b-icon-pencil></b-icon-pencil></button>
-      </div>
-      <div class="row mt-3">
-          <div class="text-right col-4 mt-2">Фамилия:</div>
-          <b-form-input class="col-3" v-model="username" readonly></b-form-input>
-          <button class="editButton col-1 ml-2"><b-icon-pencil></b-icon-pencil></button>
-      </div>
-      <div class="row mt-3">
-          <div class="text-right col-4 mt-2">Электронная почта:</div>
-          <b-form-input class="col-3" v-model="username" readonly></b-form-input>
-          <button class="editButton col-1 ml-2"><b-icon-pencil></b-icon-pencil></button>
-      </div>
-      <div class="row mt-3">
-          <div class="text-right col-4 mt-2">Город:</div>
-          <b-form-input class="col-3" v-model="username" readonly></b-form-input>
-          <button class="editButton col-1 ml-2"><b-icon-pencil></b-icon-pencil></button>
+          <b-form-input class="col-3" readonly :value="this.user.username"></b-form-input>
       </div>
       <div class="row mt-3">
           <div class="text-right col-4 mt-2">Телефон:</div>
-          <b-form-input class="col-3" v-model="username" readonly></b-form-input>
-          <button class="editButton col-1 ml-2"><b-icon-pencil></b-icon-pencil></button>
+          <b-form-input class="col-3" readonly :value="this.user.tel"></b-form-input>
       </div>
-
-      <div>Ник: {{ user.username }}</div>
-      <div>Имя: {{ user.first_name }}</div>
-      <div>Фамилия: {{ user.last_name }}</div>
-      <div>Электронная почта: {{ user.email }}</div>
-      <div>Город: {{ user.city }}</div>
-      <div>Телефон: {{ user.tel }}</div>
+      <div class="row mt-3">
+          <div class="text-right col-4 mt-2">Имя:</div>
+          <b-form-input class="col-3" v-model="user.first_name" :readonly="field_state.first_name"></b-form-input>
+          <button class="editButton col-1 ml-2" @click="field_state.first_name = !field_state.first_name; if (field_state.first_name) updateUser(); else {showSuccess = false; showDismissibleAlert = false}"><b-icon-pencil></b-icon-pencil></button>
+      </div>
+      <div class="row mt-3">
+          <div class="text-right col-4 mt-2">Фамилия:</div>
+          <b-form-input class="col-3" v-model="user.last_name" :readonly="field_state.last_name"></b-form-input>
+          <button class="editButton col-1 ml-2" @click="field_state.last_name = !field_state.last_name; if (field_state.last_name) updateUser(); else {showSuccess = false; showDismissibleAlert = false}"><b-icon-pencil></b-icon-pencil></button>
+      </div>
+      <div class="row mt-3">
+          <div class="text-right col-4 mt-2">Электронная почта:</div>
+          <b-form-input class="col-3" v-model="user.email" :readonly="field_state.email"></b-form-input>
+          <button class="editButton col-1 ml-2" @click="field_state.email = !field_state.email; if (field_state.email) updateUser(); else {showSuccess = false; showDismissibleAlert = false}"><b-icon-pencil></b-icon-pencil></button>
+      </div>
+      <div class="row mt-3">
+          <div class="text-right col-4 mt-2">Город:</div>
+          <b-form-input class="col-3" v-model="user.city" :readonly="field_state.city"></b-form-input>
+          <button class="editButton col-1 ml-2" @click="field_state.city = !field_state.city; if (field_state.city) updateUser(); else {showSuccess = false; showDismissibleAlert = false}"><b-icon-pencil></b-icon-pencil></button>
+      </div>
     </div>
   </div>
   <div>
@@ -88,6 +81,15 @@ export default {
       username: null,
       adverts: null,
       user:null,
+      field_state: {
+          first_name: true,
+          last_name: true,
+          email: true,
+          city: true
+      },
+      err: null,
+      showDismissibleAlert: false,
+      showSuccess: false,
     }
   },
   components: {
@@ -100,6 +102,19 @@ export default {
     this.$api
       .get('/api/adverts/')
       .then(response => (this.adverts = response.data));
+  },
+  methods: {
+    updateUser() {
+      let token = this.$store.state.accessToken;
+      this.$api.patch('/api/profiles/'+this.id+'/', this.user, { headers: { Authorization: "Bearer " + token } })
+        .then(() => {
+            this.showSuccess = true;
+        })
+        .catch(err => {
+            this.err = err.request.response,
+            this.showDismissibleAlert = true;
+        })
+    }
   }
 }
 </script>
